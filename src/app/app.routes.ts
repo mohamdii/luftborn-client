a7d57989882538cf8462697fcdfdc9b7d3bfdef0
mohamdii <122834211+mohamdii@users.noreply.github.com>
2026-09-05 22:01:35 +0300

    rearranging routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,38 @@ import { ProductForm } from './features/products/pages/product-form/product-form
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'products', component: ProductList, canActivate: [MsalGuard] },
-  { path: 'products/new', component: ProductForm, canActivate: [MsalGuard] },
-  { path: 'products/edit/:id', component: ProductForm, canActivate: [MsalGuard] },
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'not-found', loadComponent: () => import('./shared/notfound/not-found').then((m) => m.NotFound) },
-  { path: '**', redirectTo: 'not-found' },
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'products/new',
+    component: ProductForm,
+    canActivate: [MsalGuard],
+  },
+
+  {
+    path: 'products/edit/:id',
+    component: ProductForm,
+    canActivate: [MsalGuard],
+  },
+
+  {
+    path: 'products',
+    component: ProductList,
+    canActivate: [MsalGuard],
+  },
+
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./shared/notfound/not-found').then((m) => m.NotFound),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  },
 ];
