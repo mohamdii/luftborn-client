a558972c5a246f064afcd1a4d0c8ec822f94b5d5
mohamdii <122834211+mohamdii@users.noreply.github.com>
2026-09-05 20:05:05 +0300

    feat: Implement Not Found page with routing and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,5 +8,6 @@ export const routes: Routes = [
   { path: 'products/new', component: ProductForm, canActivate: [MsalGuard] },
   { path: 'products/edit/:id', component: ProductForm, canActivate: [MsalGuard] },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
-  {path:'not-found', loadComponent: () => import('./shared/notfound/not-found').then(m => m.NotFound)},
+  { path: 'not-found', loadComponent: () => import('./shared/notfound/not-found').then((m) => m.NotFound) },
+  { path: '**', redirectTo: 'not-found' },
 ];
--- src/app/shared/notfound/not-found.ts
+++ src/app/shared/notfound/not-found.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-not-found',
   styleUrl: './not-found.css',
   templateUrl: './not-found.html',
